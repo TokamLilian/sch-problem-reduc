6aba1d440564a0a19ec7da124b06f2ac28c3c0c1
TokamLilian <tokamlilian@gmail.com>
2023-11-22 06:51:59 -0500

    Removed all tkinter uses/edit for this branch main

--- src/app.py
+++ src/app.py
@@ -5,7 +5,6 @@ import threading
 import webbrowser
 
 from flask import Flask, jsonify, render_template
-from collections import OrderedDict
 
 import subprocess
 
@@ -37,32 +36,23 @@ def get_semester():
 def serve_index():
     return render_template('index.html')
 
-def run_tkinter_app():
-    try:
-        subprocess.run([sys.executable, 'tkinter_app.py'], check=True)
-        return 'Tkinter app opened successfully.'
-    except Exception as e:
-        print('Error running program', str(e))
 
+def open_browser():
+    webbrowser.open_new_tab("http://127.0.0.1:5001/")
 
-# def open_browser():
-#     webbrowser.open_new_tab("http://127.0.0.1:5001/")
 
 if __name__ == '__main__':
     # Start Flask app in a separate thread
     # app_thread = threading.Thread(target=app.run, kwargs={'debug': True})
     # app_thread.start()
 
-    # tkinter_timer = threading.Timer(1, run_tkinter_app)
-    # tkinter_timer.start()
-
-    # webbrowser_timer = threading.Timer(1, open_browser)
-    # webbrowser_timer.start()
+    webbrowser_timer = threading.Timer(1, open_browser)
+    webbrowser_timer.start()
 
     app.run(debug=False, port=5001)
 
 ##---Execute get_semester() ---##
-## curl http://127.0.0.1:5001/api/_get_semester ##
+## curl http://127.0.0.1:5001/api/get_semester ##
 
 
 ##---Execute run_program() from seperate terminal ---##
