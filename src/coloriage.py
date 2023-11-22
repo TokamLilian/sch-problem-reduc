1ae84f3bbc08ee2a5a8fd224b260d87043762327
TokamLilian <tokamlilian@gmail.com>
2023-11-22 15:08:27 -0500

    Finished with minimal coloring

--- src/coloriage.py
+++ src/coloriage.py
@@ -7,12 +7,31 @@ def coloriage_minimal(session):
     cours_session = {}
 
     def getCoursId(courseName):
+        """
+        Renvoie le f'cours{id}' du cours correspondant au nom passé en paramètre.
+        """
         for cours in cours_session:
             courX = cours_session[cours]
             if courX["nom"] == courseName :
                 return cours
         
         return None
+
+
+    def getCouleursDesAdjacences(cours):
+        """
+        Retourne un tableau contenant les couleurs des cours adjacents au cour
+        """
+        CouleursDesAdjacences = []
+        adjacences = cours["adjacences"]
+
+        for adjacence in adjacences:
+            adjacenceCoursId = getCoursId(adjacence)
+            adjacenceCours = cours_session[adjacenceCoursId]
+            # On ajoute une couleur pour chaque module du cours
+            CouleursDesAdjacences.append(adjacenceCours["couleur"])
+
+        return CouleursDesAdjacences
 
 
     def appendColors():
@@ -26,7 +45,18 @@ def coloriage_minimal(session):
                 coursAdjacent = cours_session[coursAdjacentId]
 
                 if cours["couleur"] == coursAdjacent["couleur"]:
-                    coursAdjacent["couleur"] = cours["couleur"] + 1
+                    #avant de d'affecter une coleur, on s'assure que 
+                    #aucun cours adjacent au cours adjacent actuel n'a deja cette couleur
+                    #sinon on donne a l'adjacence une couleur de valeur max(color de ses adjacence) + 1
+                    #s'il n'y a pas la couleur parmi ses adjacences, on peut lui donner la couleur ci dessous
+                    couleursDesAdjacencesDeCoursAdjacent = getCouleursDesAdjacences(coursAdjacent)
+                    if cours["couleur"] in couleursDesAdjacencesDeCoursAdjacent:
+                        coursAdjacent["couleur"] = max(couleursDesAdjacencesDeCoursAdjacent) + 1
+                        #TODO :si le cours adjacent n'est pas adjacent au cours 0, on peut lui donner la couleur 0
+                        #TODO :sinon si le cours adjacent n'est pas adjacent au cours 1, on peut lui donner la couleur 1
+                        #ainsi de suite...
+                    else:
+                        coursAdjacent["couleur"] = cours["couleur"] + 1
 
         return cours_session
 
