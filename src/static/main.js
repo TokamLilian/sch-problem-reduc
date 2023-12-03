d98f8056170f3106299b39d52d712f7f2d3b4fcd
TokamLilian <tokamlilian@gmail.com>
2023-12-02 23:10:20 -0500

    Added adjacences popup for nodes and style changing for arcs when hovered

--- src/static/main.js
+++ src/static/main.js
@@ -59,7 +59,7 @@ fetchData();
 
 function next(){
     const sortedMap = new Map(Object.entries(semesterData).sort(([, a], [, b]) => a.id - b.id));
-    
+
     function getId(courseName){
         let id=-1;
         for (const c in semesterData){
@@ -88,6 +88,7 @@ function next(){
     // Function to draw a course node
     function drawCourseNode(course, x, y) {
         const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        circle.setAttribute('id', 'cours'+course['id']);
         circle.setAttribute('cx', x);
         circle.setAttribute('cy', y);
         circle.setAttribute('r', 10);
@@ -106,14 +107,17 @@ function next(){
     }
 
     // Function to draw an arc between two courses
-    function drawArc(x1, y1, x2, y2) {
+    function drawArc(x1, y1, x2, y2, n) {
         const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        line.setAttribute('id', n);
+        line.setAttribute('class', 'arc');
+
         line.setAttribute('x1', x1);
         line.setAttribute('y1', y1);
         line.setAttribute('x2', x2);
         line.setAttribute('y2', y2);
         line.setAttribute('stroke', 'gray');
-        line.setAttribute('stroke-width', 1);
+        line.setAttribute('stroke-width', 2);
 
         const startDot = createDot(x1, y1);
         const endDot = createDot(x2, y2);
@@ -143,15 +147,121 @@ function next(){
     } 
 
     // Draw arcs for dependencies
-    let j = 0;
+    let numArcs = 0;
     for (const courseId of sortedMap.keys()) {
         if (courseId.startsWith("cours")) {
             const course = sortedMap.get(courseId);
             course.adjacences.forEach(dependency => {
                 const id = getId(dependency);
-                drawArc(circles[course["id"]][0].getAttribute('cx'), circles[course["id"]][0].getAttribute('cy'), circles[id][0].getAttribute('cx'), circles[id][0].getAttribute('cy'));
+                drawArc(circles[course["id"]][0].getAttribute('cx'), circles[course["id"]][0].getAttribute('cy'), circles[id][0].getAttribute('cx'), circles[id][0].getAttribute('cy'), numArcs);
+                numArcs += 1;
             });
-            j += 1;
         }
     }
+    dynamics(sortedMap);
+}
+
+
+function dynamics(sortedMap){ 
+    function getName(id){
+
+    }
+
+    function adjacencesToString(courseId){
+        //return sortedMap.get(courseId).adjacences.map((id) => {return getName(id)}).join(", ");
+        return sortedMap.get(courseId).adjacences;
+    } 
+
+    // Attach an event listener to the common ancestor
+    linesContainer.addEventListener('mouseover', function(event) {
+        // Get the target element that triggered the event
+        var target = event.target;
+
+        // Check if the target is a line element
+        if (target.tagName === 'line') {
+        // Change the style of the hovered line
+        target.style.stroke = '#4f4c4c';
+        target.style.strokeWidth = '3';
+
+        // Change the style of other lines with the shared class
+        var dataLines = document.querySelectorAll('.arc');
+        dataLines.forEach(function(line) {
+            if (line !== target) {
+            line.style.stroke = '#b8b6b6';
+            line.style.strokeWidth = '1';
+            }
+        });
+        }
+    });
+
+    linesContainer.addEventListener('mouseout', function(event) {
+        // Restore styles when the mouse leaves the line
+        var target = event.target;
+        if (target.tagName === 'line') {
+        target.style.stroke = 'gray';
+        target.style.strokeWidth = '2';
+
+        // Restore styles of other lines with the shared class
+        var dataLines = document.querySelectorAll('.arc');
+        dataLines.forEach(function(line) {
+            line.style.stroke = 'gray'; // Use the original color of your data lines
+            line.style.strokeWidth = '2'; // Use the original stroke width of your data lines
+        });
+        }
+    });
+
+  
+    var popup; // Variable to store the popup element
+
+    circlesContainer.addEventListener('mouseover', function(event) {
+        // Make sure we eork only with circles
+        if (event.target.tagName == "circle"){
+
+            const target = event.target;
+            const id = target["id"];
+
+            // Create a div element for the popup
+            popup = document.createElement('div');
+
+            // Set initial properties for the popup
+            popup.className = 'popup';
+            popup.style.display = 'none'; // Initially hide the popup
+
+            // Get the coordinates of the hover event
+            var x = event.clientX;
+            var y = event.clientY;
+
+            // Set the position of the popup near the hover coordinates
+            popup.style.left = x + 'px';
+            popup.style.top = y + 'px';
+
+            // Set the content of the popup
+            popup.textContent = "Adjacences: " + "\n" + adjacencesToString(id);
+
+            // Append the popup to the body
+            document.body.appendChild(popup);
+            
+            // Show the popup
+            popup.style.display = 'block';
+        }
+    });
+
+    circlesContainer.addEventListener('mousemove', function(event) {
+        // Update the position of the popup as the mouse moves
+        if (popup) {
+        var x = event.clientX;
+        var y = event.clientY;
+
+        popup.style.left = x + 'px';
+        popup.style.top = y + 'px';
+        }
+    });
+
+    circlesContainer.addEventListener('mouseout', function() {
+        // Remove the popup when the mouse leaves the SVG container
+        if (popup) {
+        document.body.removeChild(popup);
+        popup = null;
+        }
+    });
 }
